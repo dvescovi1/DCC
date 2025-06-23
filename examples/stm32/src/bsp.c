aa5f1dc5d5ecab26bb998fded0536681fd14c8b1
Your Name <youremail@yourdomain.com>
2025-06-23 09:09:02 -0400

    CS building

--- examples/stm32/src/bsp.c
+++ examples/stm32/src/bsp.c
@@ -3,14 +3,19 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <sys/time.h>
-#include "stm32h5xx_hal.h"
-#include "stm32h5xx_ll_cortex.h"
-#include "stm32h5xx_ll_gpio.h"
+#include "stm32h5xx_ll_icache.h"
 #include "stm32h5xx_ll_pwr.h"
+#include "stm32h5xx_ll_crs.h"
 #include "stm32h5xx_ll_rcc.h"
+#include "stm32h5xx_ll_bus.h"
+#include "stm32h5xx_ll_system.h"
+#include "stm32h5xx_ll_exti.h"
+#include "stm32h5xx_ll_cortex.h"
+#include "stm32h5xx_ll_utils.h"
+#include "stm32h5xx_ll_dma.h"
 #include "stm32h5xx_ll_tim.h"
 #include "stm32h5xx_ll_usart.h"
-#include "stm32h5xx_ll_utils.h"
+#include "stm32h5xx_ll_gpio.h"
 
 // LEDs
 #define LD1_Pin LL_GPIO_PIN_0
@@ -98,9 +103,10 @@ int _write(int, char* ptr, int len) {
 
 void SysTick_Handler() { HAL_IncTick(); }
 
-void HAL_MspInit() { __HAL_RCC_SYSCFG_CLK_ENABLE(); }
+//void HAL_MspInit() { HAL_PWREx_DisableUCPDDeadBattery();; }
 
 // Initialize system core clock to 64MHz
+#if 0
 static void init_clock() {
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
   while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_1) {}
@@ -128,47 +134,134 @@ static void init_clock() {
   // Update the time base
   if (HAL_InitTick(TICK_INT_PRIORITY) != HAL_OK) _exit(-1);
 }
+#endif
+
+
+// Initialize system core clock to 64MHz
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_1)
+  {
+  }
+
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE3);
+  while (LL_PWR_IsActiveFlag_VOS() == 0)
+  {
+  }
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
+  }
+
+  LL_RCC_HSI_SetCalibTrimming(64);
+  LL_RCC_HSI_SetDivider(LL_RCC_HSI_DIV_2);
+  LL_RCC_CSI_Enable();
+
+   /* Wait till CSI is ready */
+  while(LL_RCC_CSI_IsReady() != 1)
+  {
+  }
+
+  LL_RCC_CSI_SetCalibTrimming(32);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  {
+  }
+
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetAPB3Prescaler(LL_RCC_APB3_DIV_1);
+
+  LL_Init1msTick(32000000);
+
+  LL_SetSystemCoreClock(32000000);
+  LL_RCC_ConfigMCO(LL_RCC_MCO1SOURCE_HSI, LL_RCC_MCO1_DIV_1);
+}
+
 
 // Initialize LEDs
-static void init_gpio(void) {
-  // GPIO ports clock enable
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOA);
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOB);
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOC);
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOD);
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOE);
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOH);
-
-  LL_GPIO_ResetOutputPin(LD1_GPIO_Port, LD1_Pin);
-  LL_GPIO_ResetOutputPin(LD2_GPIO_Port, LD2_Pin);
-  LL_GPIO_ResetOutputPin(LD3_GPIO_Port, LD3_Pin);
-
+static void MX_GPIO_Init(void)
+{
   LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-  GPIO_InitStruct.Pin = LD1_Pin;
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOE);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOF);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOH);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOD);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOG);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOF, LL_GPIO_PIN_4);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_0);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOG, LL_GPIO_PIN_4);
+
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_4;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
-  LL_GPIO_Init(LD1_GPIO_Port, &GPIO_InitStruct);
+  LL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
-  GPIO_InitStruct.Pin = LD2_Pin;
-  LL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_0;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  GPIO_InitStruct.Pin = LD3_Pin;
-  LL_GPIO_Init(LD3_GPIO_Port, &GPIO_InitStruct);
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_4;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_8;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_0;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 // Initialize UART3 to 115.2k
-static void init_uart(void) {
-  LL_RCC_SetUSARTClockSource(LL_RCC_USART234578_CLKSOURCE_PCLK1);
+static void MX_USART3_UART_Init(void)
+{
 
-  // Peripheral clock enable
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART3);
-
-  LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOD);
+  LL_USART_InitTypeDef USART_InitStruct = {0};
 
   LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-  GPIO_InitStruct.Pin = STLINK_RX_Pin | STLINK_TX_Pin;
+
+  LL_RCC_SetUSARTClockSource(LL_RCC_USART3_CLKSOURCE_PCLK1);
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART3);
+
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOD);
+  /**USART3 GPIO Configuration
+  PD8   ------> USART3_TX
+  PD9   ------> USART3_RX
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_8|LL_GPIO_PIN_9;
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
   GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
@@ -176,7 +269,6 @@ static void init_uart(void) {
   GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
   LL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-  LL_USART_InitTypeDef USART_InitStruct = {0};
   USART_InitStruct.PrescalerValue = LL_USART_PRESCALER_DIV1;
   USART_InitStruct.BaudRate = 115200;
   USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
@@ -190,21 +282,24 @@ static void init_uart(void) {
   LL_USART_SetRXFIFOThreshold(USART3, LL_USART_FIFOTHRESHOLD_1_8);
   LL_USART_DisableFIFO(USART3);
   LL_USART_ConfigAsyncMode(USART3);
-
   LL_USART_Enable(USART3);
 
-  // Polling USART3 initialisation
-  while ((!(LL_USART_IsActiveFlag_TEACK(USART3))) ||
-         (!(LL_USART_IsActiveFlag_REACK(USART3)))) {}
 }
 
 //
 static void bsp_common_init(void) {
+  /* System interrupt init*/
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),15, 0));
+
   HAL_Init();
 
-  init_clock();
-  init_uart();
-  init_gpio();
+  SystemClock_Config();
+//  if (HAL_InitTick(TICK_INT_PRIORITY) != HAL_OK) _exit(-1);  MX_USART3_UART_Init();
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
 }
 
 void bsp_init_decoder(void) {
